b1e8807cc1b2d87351b8200193c831b941f7bce8
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-22 09:38:28 +0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSomething();
+        }
+
+        static void PrintSomething()
+        {
+            Console.WriteLine("PrintSomething() method is called.");
+        }
+    }
+}
